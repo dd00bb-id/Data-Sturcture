852bbc84c112f2e07cf9afcab75ab6be4f74ef9d
dd00bb-id <dnsl0630@naver.com>
2025-01-14 19:09:42 +0900

    Create stack.h

---
+++ folder/stack.h
@@ -0,0 +1,12 @@
+[0000]    class Stack {
+[0001]    public:
+[0002]        Stack ();
+[0003]        bool IsEmpty();
+[0004]        bool IsFull();
+[0005]        bool Push(int el);
+[0006]        int Pop();
+[0007]        int Top();
+[0008]    private:
+[0009]        node * top;
+[0010]    };
+[0011]    
